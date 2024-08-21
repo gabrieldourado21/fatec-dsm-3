11883fc3c797ea19e60f809f6c11aeb4e2436df2
Gabriel Dourado dos Santos <gabrieldsestudos@gmail.com>
2024-08-21 19:28:47 -0300

    Add files via upload

--- Fatec_DSM_3/TecProg2/Aulas/Aula_16_08_24/PadroesCriacionais/Builder/Builder01.js
+++ Fatec_DSM_3/TecProg2/Aulas/Aula_16_08_24/PadroesCriacionais/Builder/Builder01.js
@@ -1,85 +1,85 @@
 // Definindo as Partes de um Carro
 
 class Motor{
-  constructor(tipo){
-    this.tipo = tipo;
-  }
+    constructor(tipo){
+      this.tipo = tipo;
+    }
 }
 
 class Carroceria{
-  constructor(estilo){
-    this.estilo = estilo;
-  }
+    constructor(estilo){
+      this.estilo = estilo;
+    }
 }
 
 class Rodas{
-  constructor(tamanho){
-    this.tamanho = tamanho;
-  }
+    constructor(tamanho){
+      this.tamanho = tamanho;
+    }
 }
 
 class Aparencia{
-  constructor(cor){
-    this.cor = cor;
-  }
+    constructor(cor){
+      this.cor = cor;
+    }
 }
 
 // Builder
 
 class CarroBuilder{
-  constructor(){
-    this.motor = null;
-    this.carroceria = null;
-    this.rodas = null;
-    this.aparencia = null;
-  }
+    constructor(){
+      this.motor = null;
+      this.carroceria = null;
+      this.rodas = null;
+      this.aparencia = null;
+    }
   
-  adicionarMotor(tipo){
-    this.motor = new Motor(tipo);
-    return this;
-  }
-  
-  adicionarCarroceria(estilo){
-    this.carroceria = new Carroceria(estilo);
-    return this;
-  }
-  
-  adicionarRodas(tamanho){
-    this.rodas = new Rodas(tamanho);
-    return this;
-  }
-  
-  adicionarAparencia(cor){
-    this.aparencia = new Aparencia(cor);
-    return this;
-  }
-  
-  construir(){
-    return new Carro(
-      this.motor, 
-      this.carroceria, 
-      this.rodas, 
-      this.aparencia);
-  }
+    adicionarMotor(tipo){
+      this.motor = new Motor(tipo);
+      return this;
+    }
+    
+    adicionarCarroceria(estilo){
+      this.carroceria = new Carroceria(estilo);
+      return this;
+    }
+    
+    adicionarRodas(tamanho){
+      this.rodas = new Rodas(tamanho);
+      return this;
+    }
+    
+    adicionarAparencia(cor){
+      this.aparencia = new Aparencia(cor);
+      return this;
+    }
+    
+    construir(){
+      return new Carro(
+        this.motor, 
+        this.carroceria, 
+        this.rodas, 
+        this.aparencia);
+    }
 }
 
 // Construindo um Carro
 
 class Carro{
-  constructor(motor, carroceria, rodas, aparencia){
-    this.motor = motor;
-    this.carroceria = carroceria;
-    this.rodas = rodas;
-    this.aparencia = aparencia;
-  }
+    constructor(motor, carroceria, rodas, aparencia){
+      this.motor = motor;
+      this.carroceria = carroceria;
+      this.rodas = rodas;
+      this.aparencia = aparencia;
+    }
   
-  mostrarDetalhes(){
-    console.log(`Detalhes do Carro
-    Motor: ${this.motor.tipo}
-    Carroceria: ${this.carroceria.estilo}
-    Rodas: ${this.rodas.tamanho}
-    Aparência: ${this.aparencia.cor}\n`);
-  }
+    mostrarDetalhes(){
+      console.log(`Detalhes do Carro
+      Motor: ${this.motor.tipo}
+      Carroceria: ${this.carroceria.estilo}
+      Rodas: ${this.rodas.tamanho}
+      Aparência: ${this.aparencia.cor}\n`);
+    }
 }
 
 // Interface de Utilização
@@ -88,19 +88,18 @@ class Carro{
 const builder = new CarroBuilder();
 
 const carro1 = builder
-  .adicionarMotor('1.4')
-  .adicionarCarroceria('Sedan')
-  .adicionarRodas('14')
-  .adicionarAparencia('Preto')
-  .construir();
+    .adicionarMotor('1.4')
+    .adicionarCarroceria('Sedan')
+    .adicionarRodas('14')
+    .adicionarAparencia('Preto')
+    .construir();
 
 const carro2 = builder
-  .adicionarMotor('Eletrico')
-  .adicionarCarroceria('Pick-up')
-  .adicionarRodas('15')
-  .adicionarAparencia('Vermelho')
-  .construir();
+    .adicionarMotor('Eletrico')
+    .adicionarCarroceria('Pick-up')
+    .adicionarRodas('15')
+    .adicionarAparencia('Vermelho')
+    .construir();
 
 carro1.mostrarDetalhes();
 carro2.mostrarDetalhes();
-
